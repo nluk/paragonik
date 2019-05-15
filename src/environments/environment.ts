164a2decb6ad069d22302596365a0f11f0f0390e
nluk <nowaklukasz1995@gmail.com>
2019-05-15 13:24:16 +0200

    Changed old API key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,18 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false,
-  firebase: {
-    apiKey: "AIzaSyBWVLnqhiNHSw3ZfEVQ2r6ad0VUR1nxTaY",
-    authDomain: "paragoniki.firebaseapp.com",
-    databaseURL: "https://paragoniki.firebaseio.com",
-    projectId: "paragoniki",
-    storageBucket: "paragoniki.appspot.com",
-    messagingSenderId: "270606546382",
-    appId: "1:270606546382:web:d121c337959d47c4"
-  }
-};
+
 
 /*
  * For easier debugging in development mode, you can import the following file
