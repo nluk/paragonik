20e50be5f9036ba7e7eba2c129672a9dc8bbb5df
nluk <nowaklukasz1995@gmail.com>
2019-05-15 14:16:29 +0200

    Finished login, registration, dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,29 @@ import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { environment } from 'src/environments/environment';
+import { AuthenticateService } from './services/authentication.service';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+import * as firebase from 'firebase';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
+firebase.initializeApp(environment.firebase);
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [ReactiveFormsModule, BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireAuthModule],
   providers: [
     StatusBar,
     SplashScreen,
+    AuthenticateService,
     Camera,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
