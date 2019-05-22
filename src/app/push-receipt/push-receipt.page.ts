c6bfef9494174b7e4ad66f217cfd8314b631b7be
Mateusz <matkro9@gmail.com>
2019-05-22 12:22:26 +0200

    zdjecie 2 test

--- src/app/push-receipt/push-receipt.page.ts
+++ src/app/push-receipt/push-receipt.page.ts
@@ -7,11 +7,11 @@ import { Camera, CameraOptions } from "@ionic-native/camera/ngx"
   styleUrls: ['./push-receipt.page.scss'],
 })
 export class PushReceiptPage implements OnInit {
-  public base64image: string;
+  public base64image: string = null;
 
   options: CameraOptions = {
     quality: 100,
-    destinationType: this.camera.DestinationType.FILE_URI,
+    destinationType: this.camera.DestinationType.DATA_URL,
     encodingType: this.camera.EncodingType.JPEG,
     mediaType: this.camera.MediaType.PICTURE
   };
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyCBKzLz-XadmbnNV6v_70Wplexlj40-_10",
+        authDomain: "paragonikk.firebaseapp.com",
+        databaseURL: "https://paragonikk.firebaseio.com",
+        projectId: "paragonikk",
+        storageBucket: "paragonikk.appspot.com",
+        messagingSenderId: "881081584146",
+        appId: "1:881081584146:web:6e69b1e82c4f4c54"
+    }
+};
