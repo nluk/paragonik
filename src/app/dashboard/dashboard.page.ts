2c257e33ca9bac0fcbb12bbc45923d26810384f9
nluk <nowaklukasz1995@gmail.com>
2019-06-02 17:33:54 +0200

    Modified dashboard display

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController, ModalController } from '@ionic/angular';
 import { AuthenticateService } from '../services/authentication.service';
+import { ReceiptService } from './../services/receipt.service';
+import { Receipt } from 'src/app/push-receipt/receipt'
 
 @Component({
   selector: 'app-dashboard',
@@ -11,9 +13,11 @@ export class DashboardPage implements OnInit {
 
 
   userEmail: string;
+  receipts: any;
 
   constructor(
     private navCtrl: NavController,
+    private receiptService: ReceiptService,
     private authService: AuthenticateService
   ) { }
 
@@ -21,9 +25,29 @@ export class DashboardPage implements OnInit {
 
     if (this.authService.userDetails()) {
       this.userEmail = this.authService.userDetails().email;
+
+      this.receiptService.read_Receipts(this.userEmail).subscribe(data => {
+
+        this.receipts = data.map(e => {
+          return {
+            id: e.payload.doc.id,
+            title: e.payload.doc.data()['title'],
+            description: e.payload.doc.data()['description'],
+            imageURL: e.payload.doc.data()['imageURL'],
+          };
+        })
+        console.log(this.receipts);
+
+      });
+
+
+
     } else {
       this.navCtrl.navigateBack('');
     }
+
+
+
   }
 
   addItem() {
